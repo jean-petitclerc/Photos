2e63b902a602631f5f8a201d5bad770d92577cd9
Jean Petitclerc <jean.petitclerc@groupepp.com>
2015-06-09 20:22:50 -0400

    Incomplete version of std_date_folders.py.

---
+++ std_date_folders.py
@@ -0,0 +1,31 @@
+__author__ = 'jean'
+# -*- coding: utf-8 -*-
+
+import sys, os
+
+def scan_dir(start_dir):
+    try:
+        for root, dirs, files in os.walk(start_dir):
+            for dir in dirs:
+                print("Found this directory: %s" % dir)
+    except OSError as e:
+        print("OS error: {0}".format(e))
+
+
+def main():
+    if (len(sys.argv) - 1) < 2:
+        print("Ce programme a besoin de deux arguments, le dossier de depart et le format de date MDY ou DMY.")
+        return 8
+    start_dir = sys.argv[1]
+    cur_dt_fmt = sys.argv[2]
+    cur_dt_fmt = cur_dt_fmt.upper()
+    if cur_dt_fmt not in ['DMY', 'MDY']:
+        print("Le format doit etre MDY ou DMY")
+        return 8
+    print("Dossier de depart: %s" % start_dir)
+    print("Format initial...: %s" % cur_dt_fmt)
+    scan_dir(start_dir)
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
